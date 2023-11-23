d15ccf40f4c93b871f7119212f7927060cf8bb88
BraydenNickel <brayden.nickel@edu.sait.ca>
2023-11-23 14:52:38 -0700

    Stack and Queue Tests

--- ADTsAndXML/tests/unitTests/MyQueueTests.java
+++ ADTsAndXML/tests/unitTests/MyQueueTests.java
@@ -5,21 +5,27 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.util.EmptyStackException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.project.classes.MyQueue;
 
 /**
  * @author Ruthless
  *
  */
-public class MyQueueTests {
+public class MyQueueTests<E> {
+	private MyQueue<Integer> myQueue;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		myQueue = new MyQueue<>();
 	}
 
 	/**
@@ -27,11 +33,59 @@ public class MyQueueTests {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		myQueue = null;
 	}
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testEnqueue() {
+        assertTrue(myQueue.isEmpty());
+        myQueue.enqueue(1);
+        assertFalse(myQueue.isEmpty());
+        assertEquals(1, myQueue.size());
+        assertEquals(Integer.valueOf(1), myQueue.peek());
+    }
 
+    @Test
+    public void testDequeue() {
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        assertEquals(2, myQueue.size());
+        assertEquals(Integer.valueOf(1), myQueue.dequeue());
+        assertEquals(1, myQueue.size());
+        assertEquals(Integer.valueOf(2), myQueue.peek());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testDequeueEmptyQueue() {
+        myQueue.dequeue();
+    }
+
+    @Test
+    public void testPeek() {
+        myQueue.enqueue(1);
+        myQueue.enqueue(2);
+        assertEquals(Integer.valueOf(1), myQueue.peek());
+        assertEquals(2, myQueue.size());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testPeekEmptyQueue() {
+        myQueue.peek();
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(myQueue.isEmpty());
+        myQueue.enqueue(1);
+        assertFalse(myQueue.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, myQueue.size());
+        myQueue.enqueue(1);
+        assertEquals(1, myQueue.size());
+        myQueue.enqueue(2);
+        assertEquals(2, myQueue.size());
+    }
 }
--- ADTsAndXML/tests/unitTests/MyStackTests.java
+++ ADTsAndXML/tests/unitTests/MyStackTests.java
@@ -5,33 +5,82 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.util.EmptyStackException;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.project.classes.MyStack;
 
 /**
  * @author Ruthless
  *
  */
-public class MyStackTests {
+public class MyStackTests<E> {
+	private MyStack<Integer> myStack;
+	
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@Before
 	public void setUp() throws Exception {
+		myStack = new MyStack<>();
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
 	@After
 	public void tearDown() throws Exception {
+		myStack = null;
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+    public void testPush() {
+        assertTrue(myStack.isEmpty());
+        myStack.push(1);
+        assertFalse(myStack.isEmpty());
+        assertEquals(1, myStack.size());
+        assertEquals(Integer.valueOf(1), myStack.peek());
+    }
 
+    @Test
+    public void testPop() {
+        myStack.push(1);
+        myStack.push(2);
+        assertEquals(2, myStack.size());
+        assertEquals(Integer.valueOf(2), myStack.pop());
+        assertEquals(1, myStack.size());
+        assertEquals(Integer.valueOf(1), myStack.peek());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testPopEmptyStack() {
+        myStack.pop();
+    }
+
+    @Test
+    public void testPeek() {
+        myStack.push(1);
+        myStack.push(2);
+        assertEquals(Integer.valueOf(2), myStack.peek());
+        assertEquals(2, myStack.size());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testPeekEmptyStack() {
+        myStack.peek();
+    }
+
+    @Test
+    public void testIsEmpty() {
+        assertTrue(myStack.isEmpty());
+        myStack.push(1);
+        assertFalse(myStack.isEmpty());
+    }
+
+    @Test
+    public void testSize() {
+        assertEquals(0, myStack.size());
+        myStack.push(1);
+        assertEquals(1, myStack.size());
+        myStack.push(2);
+        assertEquals(2, myStack.size());
+    }
 }
